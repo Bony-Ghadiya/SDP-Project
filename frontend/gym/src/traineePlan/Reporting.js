339451b5a97fb3f0a6a0f5cf99caa06451254a41
bony-ghadiya <bghadiya001@gmail.com>
2021-03-16 17:10:08 +0530

    title

--- frontend/gym/src/traineePlan/Reporting.js
+++ frontend/gym/src/traineePlan/Reporting.js
@@ -285,6 +285,7 @@ export default function ProgressMobileStepper() {
 									onChange={handleChange('other')}
 								/>
 							</div>
+							<br></br>
 							<input
 								className="btn1"
 								type="submit"
